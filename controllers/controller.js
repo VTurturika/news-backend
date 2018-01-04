a193f0a5d7d8edb6b5ecd6ff4b65795118f6ee20
Victor Turturika <v.turturika@gmail.com>
2018-01-04 17:39:12 +0200

    feat(controller): Add validation method isExistParam()

--- controllers/controller.js
+++ controllers/controller.js
@@ -7,8 +7,16 @@ class Controller {
   isExistField(req, field) {
     return new Promise((resolve, reject) => {
       return req.body && req.body[field] !== undefined
-        ? resolve()
+        ? resolve(req.body[field])
         : reject(new error.BadRequestError(`field ${field} is required`))
+    });
+  }
+
+  isExistParam(req, param) {
+    return new Promise((resolve, reject) => {
+      return req.params && req.params[param] !== undefined
+        ? resolve(req.params[param])
+        : reject(new error.BadRequestError(`param :${param} is required`))
     });
   }
 }
