a431deea539ef0aec5c67662b007242b175da643
Victor Turturika <v.turturika@gmail.com>
2018-01-04 16:52:16 +0200

    feat(controller): Implement checking required fields in request body

--- controllers/controller.js
+++ controllers/controller.js
@@ -1,7 +1,16 @@
 'use strict';
 
+const error = require('restify-errors');
+
 class Controller {
 
+  isExist(req, field) {
+    return new Promise((resolve, reject) => {
+      return req.body && req.body[field] !== undefined
+        ? resolve()
+        : reject(new error.BadRequestError(`field ${field} is required`))
+    });
+  }
 }
 
 module.exports = {
